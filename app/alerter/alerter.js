06eb6ee05480b84972ee378efbd2f92c8a507b37
Abhishek Anurag <anurag.abhishek3@gmail.com>
2014-11-17 03:39:53 -0600

    Extra cred

--- app/alerter/alerter.js
+++ app/alerter/alerter.js
@@ -19,7 +19,9 @@
 
         API_KEY: "b75409abea9550616ed31c627952fff9efe585c6",
         CORS_API_URL: "https://cors-anywhere.herokuapp.com/",
-        SENDHUB_API_URL: "https://api.sendhub.com/v1/messages/"
+        SENDHUB_API_URL: "https://api.sendhub.com/v1/messages/",
+
+        VISUALIZER_TICK_VALUES: [0.0,0.2,0.4,0.6,0.8,1.0]
     });
 
     app.controller('AlertCtrl', ['$scope', '$timeout', 'micListener', 'messenger', 'appConstants',
@@ -169,8 +171,11 @@
             }
 
             function whenDataReceived($scope, handler) {
-                $scope.$on(VOLUME_DATA_READY_EVENT, function (event, data) {
+                var cleanUp = $scope.$on(VOLUME_DATA_READY_EVENT, function (event, data) {
                     handler(data);
+                });
+                $scope.$on('$destroy', function() {
+                    cleanUp();
                 });
             }
 
@@ -207,6 +212,84 @@
                 sendMessage: sendMessage
             };
             return services;
+        }
+    ]);
+
+
+    app.controller('VisualizerCtrl',['$scope', 'micListener', 'appConstants',
+        function($scope, micListener, appConstants){
+            var n = 40,
+                random = d3.random.normal(0, .2),
+                data = d3.range(n).map(function(){
+                    return 0;
+                });
+
+            var start_y = 460*(1-appConstants.DEFAULT_THRESHOLD);
+
+            var margin = {top: 20, right: 20, bottom: 20, left: 40},
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
+
+            var x = d3.scale.linear()
+                .domain([1, n - 2])
+                .range([0, width]);
+
+            var y = d3.scale.linear()
+                .domain([0, 1])
+                .range([height, 0]);
+
+            var line = d3.svg.line()
+                .interpolate("basis")
+                .x(function(d, i) { return x(i); })
+                .y(function(d, i) { return y(d); });
+
+            var svg = d3.select("body").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            svg.append("defs").append("clipPath")
+                .attr("id", "clip")
+                .append("rect")
+                .attr("width", width)
+                .attr("height", height);
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + y(0) + ")")
+                .call(d3.svg.axis().scale(x).orient("bottom").tickValues([]));
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(d3.svg.axis().scale(y).orient("left").tickValues(appConstants.VISUALIZER_TICK_VALUES));
+
+            svg.append("path")
+                .attr("class", "line")
+                .style("stroke-dasharray", ("3, 3"))
+                .attr("d", "M0 "+start_y+" L900 "+start_y);
+
+            var path = svg.append("g")
+                .attr("clip-path", "url(#clip)")
+                .append("path")
+                .datum(data)
+                .attr("class", "line")
+                .attr("d", line);
+
+            function tick(value) {
+                data.push(value);
+
+                path
+                    .attr("d", line)
+                    .attr("transform", null)
+                    .transition()
+                    .duration(0)
+                    .ease("linear")
+                    .attr("transform", "translate(" + x(0) + ",0)")
+                data.shift();
+
+            }
+            micListener.whenDataReceived($scope, tick);
         }
     ]);
 
